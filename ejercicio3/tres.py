e93cb410c7e2c15018f14865f12523e30eb7773e
clauloro <114728388+clauloro@users.noreply.github.com>
2023-03-10 17:47:51 +0100

    .

--- ejercicio3/tres.py
+++ ejercicio3/tres.py
@@ -1,13 +1,13 @@
-def es_palindromo(frase):
-    caracteres = ''
-    for c in frase:
-        if c.isalpha():                         #para verificar si un carácter es una letra
-            caracteres += c.lower().replace('á','a').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u')
-        elif c.isdigit():                       #para verificar si un carácter es un dígito 
-            caracteres += c
-    return caracteres == caracteres[::-1]
-   
-print(es_palindromo("Hello world"))
-print(es_palindromo("Salas"))
-print(es_palindromo("oSo"))
-print(es_palindromo("26762"))
+def ordenar_fichas(fichas):
+    rojas= []
+    verdes= []
+    azules = []
+    for ficha in fichas:
+        if ficha== "rojo":
+            rojas.append(ficha)
+        elif ficha == "verde":
+            verdes.append(ficha)
+        elif ficha == "azul":
+            azules.append(ficha)
+    return rojas + verdes + azules 
+print(ordenar_fichas(["rojo","azul","azul","rojo","verde","azul","azul","rojo","azul","rojo","rojo","verde","rojo","rojo","azul","verde","verde"]))
